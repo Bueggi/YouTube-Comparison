6c8e4afe19cd19dd7381ea5d529e14802d8495f2
Christopher Buecklein <christopher_b@gmx.de>
2018-11-08 23:00:11 +0100

    made it possible to show the chart for traffic sources and made them colorful

--- youtubecomparison/src/components/pComponents/trafficSourcesComponent.js
+++ youtubecomparison/src/components/pComponents/trafficSourcesComponent.js
@@ -10,30 +10,67 @@ class TrafficSources extends Component {
     }
   }
 
+  chartColors = [
+    'rgba(216, 164, 127, 0.1)',
+    'rgba(223, 59, 87, 0.1)',
+    'rgba(15, 113, 115, 0.1)',
+    'rgba(124, 127, 101, 0.1)',
+    'rgba(203, 111, 115, 0.1)',
+    'rgba(203, 111, 115, 0.1)',
+  ];
+
+  borderColors = [
+    'rgba(216, 164, 127, 1)',
+    'rgba(223, 59, 87, 1)',
+    'rgba(15, 113, 115, 1)',
+    'rgba(124, 127, 101, 1)',
+    'rgba(203, 111, 115, 1)',
+  ];
+
+  colorCounter = 0;
+
+  datasToDisplay = [
+    'YT_SEARCH',
+    'RELATED_VIDEO',
+    'PLAYLIST',
+    'SUBSCRIBER',
+    'END_SCREEN'
+  ];
+
   dataToTrafficSource = (TSdata) => {
     const traversedData = TSdata.reduce((acc, date) => {
     if(acc.labels.indexOf(date[0]) === -1) acc.labels.push(date[0]);
 
-    const dataset = acc.datasets.find(el => el.label === date[1])
-    if (!dataset) {
-      acc.datasets.push({
-        label: date[1],
-        data: [date[2]]
-      })
+    if (this.datasToDisplay.indexOf(date[1]) !== -1) {
+
+      const dataset = acc.datasets.find(el => el.label === date[1])
+      if (!dataset) {
+        acc.datasets.push({
+          label: date[1],
+          data: [date[2]],
+          backgroundColor: this.chartColors[this.colorCounter],
+          borderColor: this.borderColors[this.colorCounter]
+        })
+        this.colorCounter++;
+      }
+      else dataset.data.push(date[2])
     }
-    else dataset.data.push(date[2])
+
 
     return acc;
   }, {
     labels: [],
-    datasets: []
+    datasets: [],
   });
 
+  console.log(traversedData);
   return traversedData;
   }
 
   componentWillMount () {
-    this.dataToTrafficSource(this.props.data);
+    this.setState({ chartData: this.dataToTrafficSource(this.props.data)});
+    console.log('//// data I get as prop', this.props.data)
+    console.log('//// chartData', this.state.chartData)
   }
 
   render () {
